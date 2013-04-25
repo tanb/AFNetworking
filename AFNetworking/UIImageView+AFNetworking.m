7b933c868e6871a01c9de596af606e786e134bf6
Tomonori Tanabe <jazzical19@mac.com>
2013-04-25 14:47:00 +0900

    refactored.

--- AFNetworking/UIImageView+AFNetworking.m
+++ AFNetworking/UIImageView+AFNetworking.m
@@ -173,18 +173,25 @@ NSInteger dateModifiedSort(id file1, id file2, void *reverse) {
 
 
 #pragma mark - memCache
+- (BOOL)removeUnretainedMemCacheForKey:(NSString *)key
+{
+    __weak id weakImage = nil;
+    @autoreleasepool {
+        weakImage = [self.memCaches objectForKey:key];
+        [self.memCaches removeObjectForKey:key];
+    }
+    
+    if (weakImage) {
+        [self.memCaches setObject:weakImage forKey:key];
+        return NO;
+    }
+    return YES;
+}
+
 - (void)removeUnretainedMemCache
 {
     for (NSString *key in [self.memCaches allKeys]) {
-        __weak id safeObject = nil;
-        @autoreleasepool {
-            safeObject = [self.memCaches objectForKey:key];
-            [self.memCaches removeObjectForKey:key];
-        }
-        
-        if (safeObject) {
-            [self.memCaches setObject:safeObject forKey:key];
-        }
+        [self removeUnretainedMemCacheForKey:key];
     }
 }
 
@@ -239,7 +246,7 @@ NSInteger dateModifiedSort(id file1, id file2, void *reverse) {
 		return nil;
 	}
 	const char *cStr = [URL UTF8String];
-	unsigned char result[16];
+	unsigned char result[CC_MD5_DIGEST_LENGTH];
 	CC_MD5(cStr, (CC_LONG)strlen(cStr), result);
 	return [NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
             result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],result[8], result[9], result[10], result[11],result[12], result[13], result[14], result[15]];
